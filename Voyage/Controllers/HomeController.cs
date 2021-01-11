86d510c6f5b2f44f4855182c7eedbc705a2c7aba
walidS <walidsousside@gmail.com>
2021-01-12 00:13:28 +0100

    Migration

---
+++ Voyage/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Voyage.Models;
+
+namespace Voyage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        string[] ListeVille = {
+            "Volvo",
+            "BMW",
+            "Ford",
+            "Mazda"
+        };
+        string[] ListeOptionBus = {
+            "Wifi",
+            "Clime",
+            "Wc",
+            "Tv"
+        };
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            @ViewBag.SlectedView = "Home" ;
+            @ViewBag.ListeVille = ListeVille ;
+            @ViewBag.ListeOptionBus = ListeOptionBus;
+            Console.WriteLine(@ViewBag.SlectedView);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Voyage/Controllers/OffreController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Voyage.Views.Offre
+{
+    public class OffreController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            @ViewBag.SlectedView = "Offre";
+            Console.WriteLine(@ViewBag.SlectedView);
+            return View();
+        }
+    }
+}
